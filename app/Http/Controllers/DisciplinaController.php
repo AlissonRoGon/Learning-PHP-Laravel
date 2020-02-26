b583ed1ff1a9cd45886226434f93fd2056509d32
Alisson Rodrigo Gonçalves <alissonrogon@gmail.com>
2020-02-26 16:15:03 -0300

    issue 9 associando disciplina a uma turma

--- app/Http/Controllers/DisciplinaController.php
+++ app/Http/Controllers/DisciplinaController.php
@@ -92,4 +92,22 @@ class DisciplinaController extends Controller
         $disciplina->delete();
         return redirect("/");
     }
+
+    public function createTurma($disciplina_id)
+    {
+        return view('disciplinas.turmas.create',compact('disciplina_id'));
+    }
+
+    public function storeTurma(Request $request, $disciplina_id)
+    {
+        $turma = new \App\Turma;
+        $turma->ministrante = $request->ministrante;
+        $turma->inicio = $request->inicio;
+        $turma->fim = $request->fim;
+        $turma->bibliografia = $request->bibliografia;
+        $turma->disciplina_id = $request->disciplina_id;
+        Disciplina::find($disciplina_id)->turmas()->save($turma);
+        return redirect("/disciplinas/$disciplina_id");
+    }
+
 }
