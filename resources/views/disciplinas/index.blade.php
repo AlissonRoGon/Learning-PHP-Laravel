894ad73f3667da543c8d28e8b85957e477a4148e
Alisson Rodrigo Gonçalves <alissonrogon@gmail.com>
2020-02-26 15:18:16 -0300

    edit disciplina

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -8,6 +8,8 @@
             <a href="/disciplinas/{{$disciplina->id}}">
                 {{$disciplina->titulo }}
             </a>
+            <br>
+            <a href="/disciplinas/{{$disciplina->id}}/edit">Editar</a>
         </li>
     @endforeach
 </ul>
