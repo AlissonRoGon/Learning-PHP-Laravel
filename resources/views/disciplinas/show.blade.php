150b2601a446f3fc9f362069cefda4da0097c68b
Alisson Rodrigo Gonçalves <alissonrogon@gmail.com>
2020-02-26 13:59:17 -0300

    Issue 3 - metodo show disciplinas

---
+++ resources/views/disciplinas/show.blade.php
@@ -0,0 +1,4 @@
+<h1>{{$disciplina->titulo}}</h1>
+<p>Ementa: {{$disciplina->ementa}}</p>
+<p>Data de criação: {{$disciplina->created_at}}</p>
+<p>Data de atualização: {{$disciplina->updated_at}}</p>
