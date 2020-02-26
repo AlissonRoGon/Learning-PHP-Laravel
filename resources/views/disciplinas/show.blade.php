febeb07d8888a9aa48825bb1f760629ae71efda0
Alisson Rodrigo Gonçalves <alissonrogon@gmail.com>
2020-02-26 17:10:31 -0300

    listar turmas que disciplina contem

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -3,6 +3,16 @@
 <p>Data de criação: {{$disciplina->created_at}}</p>
 <p>Data de atualização: {{$disciplina->updated_at}}</p>
 <br>
+    <h2>Turmas: </h2>
+    @foreach ($disciplina->turmas as $turma)
+        <div>
+            {{ $turma->ministrante }}
+            {{ $turma->inicio }}
+        </div>
+    @endforeach
+<br>
+<br>
+<br>
 <a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Inserir Turma</a>
 <br>
 <a href="/disciplinas/{{$disciplina->id}}/edit">Editar</a>
